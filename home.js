abdd7fe641a7817df6823e5f300fd5ee5f8c1410
Giorgi Rakviashvili <rakviashvili.giorgi@gmail.com>
2024-11-01 18:45:36 +0400

    testing3

--- home.js
+++ home.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
+  console.log("giorgi0...");
+  console.log("giorgi1...");
+  console.log("giorgi2...");
+  console.log("giorgi3...");
   return res.status(200).json({
     title: "Express Testing",
     message: "The app is working properly!",
