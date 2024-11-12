1df56788f5f400c7a137b016502192c1fc9edb4a
Giorgi Rakviashvili <rakviashvili.giorgi@gmail.com>
2024-11-12 17:04:13 +0400

    testing

--- js/weatherOps.js
+++ js/weatherOps.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const WeatherModel = require("../models/weather.model");
 const saveModel = require("../js/saveModel");
 
+require("dotenv").config();
+
+const url = process.env.MONGO_ADDRESS;
+
 const saveWeather = (temp, humidity) => {
   const weather = new WeatherModel({
     _id: new mongoose.Types.ObjectId(),
@@ -12,6 +16,7 @@ const saveWeather = (temp, humidity) => {
 
   // saveModel(weather);
   console.log("save", temp, humidity);
+  console.log("uri", url);
   weather.save();
 };
 
